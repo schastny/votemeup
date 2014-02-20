e219d34faab1edb5849578621bd95ec3afd0ad62
marengo75 <marengo.sys@gmail.com>
2014-02-20 17:12:55 +0200

    Create DB on HeidiSQL

---
+++ Level.sql
@@ -0,0 +1,7 @@
+/*
+-- Query: 
+-- Date: 2014-02-19 15:13
+*/
+INSERT INTO `Level_Proposal` (`id`,`Level`,`Proposal_id`) VALUES (1,'По району',NULL);
+INSERT INTO `Level_Proposal` (`id`,`Level`,`Proposal_id`) VALUES (2,'По городу',NULL);
+INSERT INTO `Level_Proposal` (`id`,`Level`,`Proposal_id`) VALUES (3,'По стране',NULL);
