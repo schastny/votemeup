892340e2a15ad13c930f1be1ea5288bdebb7508c
Ponomarenko <marengo.sys@gmail.com>
2014-03-03 17:55:50 +0200

    Add Desktop EXAMPLE

---
+++ desktop/src/main/java/net/schastny/jpa/domain/Person.java
@@ -0,0 +1,55 @@
+package net.schastny.jpa.domain;
+
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "persons")
+public class Person {
+
+    @Id
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @ManyToMany
+    @JoinTable(
+            name = "ptable", 
+            joinColumns         = @JoinColumn(name = "person_id", referencedColumnName = "id"), 
+            inverseJoinColumns     = @JoinColumn(name = "project_id", referencedColumnName = "id"))
+    private List<Project> projects;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Project> getProjects() {
+        return projects;
+    }
+
+    public void setProjects(List<Project> projects) {
+        this.projects = projects;
+    }
+
+}
---
+++ desktop/src/main/java/net/schastny/jpa/domain/Project.java
@@ -0,0 +1,63 @@
+package net.schastny.jpa.domain;
+
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "projects")
+public class Project {
+
+    @Id
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "cat_id")
+    private Category category;
+
+    @ManyToMany(mappedBy = "projects")
+    private List<Person> persons;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public List<Person> getPersons() {
+        return persons;
+    }
+
+    public void setPersons(List<Person> persons) {
+        this.persons = persons;
+    }
+
+}
