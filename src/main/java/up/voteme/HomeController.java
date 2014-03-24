6d810a011dbb63d8a6ad5b91523f2fbbb5d29014
Ponomarenko <marengo.sys@gmail.com>
2014-03-24 18:42:13 +0200

    240314 try again

--- src/main/java/up/voteme/HomeController.java
+++ src/main/java/up/voteme/HomeController.java
@@ -5,19 +5,24 @@ import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import up.voteme.dao.ProposalDAO;
-import up.voteme.domain.Proposal;
+import up.voteme.domain.ProposalStatus;
+import up.voteme.service.ProposalStatusService;
 
 /**
  * Sample controller for going to the home page with a message
  */
 @Controller
 public class HomeController {
+
+	@Autowired
+	private ProposalStatusService proposalStatusService;
+
 
 	private static final Logger logger = LoggerFactory
 			.getLogger(HomeController.class);
@@ -26,19 +31,14 @@ public class HomeController {
 	 * Selects the home page and populates the model with a message
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
+
 	public String home(Model model) {
 		logger.info("Welcome home!");
-		
-		//log all proposals for debug purposes
-		ProposalDAO dao = new ProposalDAO();
-		List<Proposal> list= dao.findAll();
-		for (Proposal p: list){
-			logger.debug(p.toString());
-		}
-		//add all proposals as attribute to show on home.jsp 
-		model.addAttribute("list",list);
-		
-		
+
+		List<ProposalStatus> psList = proposalStatusService.findAllPS();
+		model.addAttribute("psList",psList);
+
+
 		model.addAttribute("controllerMessage",
 				"This is the message from the controller! "+new Date());
 		return "home";
