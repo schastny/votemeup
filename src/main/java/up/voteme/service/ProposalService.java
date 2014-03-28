0be3400727490fb60a32f460c354556f1ffb63c7
Anthony Lebedev <anthony.lebedev@gmail.com>
2014-03-28 18:55:44 +0200

    Meeting Notes at 28.03.2014

--- src/main/java/up/voteme/service/ProposalService.java
+++ src/main/java/up/voteme/service/ProposalService.java
@@ -14,39 +14,37 @@ public interface ProposalService {
 	public abstract void delete(long id);
 
 	public abstract List<Proposal> getAllbyDate();
-	
+
 	public abstract List<Proposal> getAllbyVoteNum();
-	
+
 	// service methods for guesthomepage
-	
+
 	public abstract long countAll();
-	
-	 
-	/* 
-	   public abstract List<Proposal> getByParams (HashMap<String,String> map);
-	   
-			!!! ( 1.noSort = showAll; 2. use .equalseIgnoreCase(); 3.hint: int num = Integer.parseInt(..) )
-		 	map structure(key = value), expected parameters in curly braces :
-			 	sortBy = {noSort, voteCount, creationDate, commentCount};
-			 	pageNum = {1...countAll()/PageQuant};
-			 	pageQuant = {10,25,50};
-			 	filtrByLevel = {noSort, Collection: proposalLevel.findAll().getLevel};
-			 	filtrByStatus = {noSort, Collection: proposalStatus.findAll().getStatus};
-			 	filtrByCategory = {noSort, Collection: proposalCategory.findAll().getCategoryName()};
-			 	filtrByCountry = {noSort, Collection: country.findAll().getCountryName};
-			 	filtrByRegion = {noSort, Collection: region.findAll().get..};
-			 	filtrByCity = {noSort, Collection: city.findAll().get..};
-			 	filtrByDistrict = {noSort, Collection: district.findAll().get..};
-	 */
-	/* 
-	  Also we need:
-	  	public abstract List<Region> getByCountryName (String countryName);
-	  	public abstract List<City> getByRegionName (String regionName);
-	  	public abstract List<District> getByCityName (String cityName);
+
+	// public abstract List<Proposal> getByParams(HashMap<String, String> map);
+
+	/*
+	 * !!! ( 1.noSort = showAll; 2. use .equalseIgnoreCase(); 3.hint: int num =
+	 * Integer.parseInt(..) ) map structure(key = value), expected parameters in
+	 * curly braces : 
+	 * sortBy = {noSort, voteCount, creationDate, commentCount};
+	 * pageNum = {1..countAll() / PageQuant};
+	 * pageQuant = {10,25,50};
+	 * filtrByLevel = {noSort, Collection: proposalLevel.findAll().getLevel};
+	 * filtrByStatus = {noSort, Collection: proposalStatus.findAll().getStatus};
+	 * filtrByCategory = {noSort, Collection:
+	 * proposalCategory.findAll().getCategoryName()}; filtrByCountry = {noSort,
+	 * Collection: country.findAll().getCountryName}; filtrByRegion = {noSort,
+	 * Collection: region.findAll().get..}; filtrByCity = {noSort, Collection:
+	 * city.findAll().get..}; filtrByDistrict = {noSort, Collection:
+	 * district.findAll().get..};
 	 */
 	
-	
-	
-	
+	/*
+	 * Also we need: public abstract List<Region> getByCountryName (String
+	 * countryName); public abstract List<City> getByRegionName (String
+	 * regionName); public abstract List<District> getByCityName (String
+	 * cityName);
+	 */
 
 }
