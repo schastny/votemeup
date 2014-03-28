05cc06365ca7b36be74b2384cac4549a82974de8
Roman <roman@sferos.com>
2014-03-28 18:51:43 +0200

    Same, Part 2

---
+++ src/main/java/up/voteme/dao/CommentDAOImpl.java
@@ -0,0 +1,52 @@
+package up.voteme.dao;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import javax.persistence.TypedQuery;
+
+import org.springframework.stereotype.Component;
+import up.voteme.domain.Comment;
+
+@Component
+public class CommentDAOImpl implements CommentDAO {
+	
+	@PersistenceContext
+	EntityManager eMgr;
+	
+	@Override
+	public long store(Comment item) {                 // store record
+		long Id = eMgr.merge(item).getCommentId();
+		return Id;
+	}
+
+	@Override
+	public void delete(long CommentId) {              // delete record
+		Comment Comment = eMgr.find(Comment.class, CommentId);
+		eMgr.remove(Comment);
+	}
+
+	@Override
+	public Comment findById(long CommentId) {         // find record
+		return eMgr.find(Comment.class, CommentId);
+	}
+
+	@Override
+	public List<Comment> findAll() {                  // select all records
+		TypedQuery<Comment> query = eMgr.createQuery("SELECT com FROM Comment com", Comment.class);
+		List<Comment> items = query.getResultList();
+		for (Comment item : items) {
+			System.out.println(item);
+		}
+		return items;
+	}
+	
+	@Override
+	public long countAll() {                          // return records count
+		Query query = eMgr.createQuery("SELECT COUNT(*) FROM Comment");
+		long result = (long) query.getSingleResult();
+		return result;
+	}
+}
