933619b8c6fb50b3bf2893eba51e274900b344c3
Anthony Lebedev <anthony.lebedev@gmail.com>
2014-03-28 17:31:13 +0200

    Proposals&Level

---
+++ src/main/java/up/voteme/dao/ProposalLevelDAOImpl.java
@@ -0,0 +1,44 @@
+package up.voteme.dao;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+
+import org.springframework.stereotype.Component;
+
+import up.voteme.domain.District;
+import up.voteme.domain.ProposalLevel;
+
+@Component
+public class ProposalLevelDAOImpl implements ProposalLevelDAO {
+	@PersistenceContext
+	private EntityManager manager;
+
+	@Override
+	public long store(ProposalLevel item) {
+		long id = manager.merge(item).getId();
+		return id;
+	}
+
+	@Override
+	public void delete(Long id) {
+		ProposalLevel proposalLevel = manager.find(ProposalLevel.class,id);
+		manager.remove(proposalLevel);
+	}
+
+	@Override
+	public ProposalLevel findById(Long id) {
+		return manager.find(ProposalLevel.class, id);
+	}
+
+	@Override
+	public List<ProposalLevel> findAll() {
+		 TypedQuery<ProposalLevel> query = manager.createQuery("SELECT pl FROM ProposalLevel pl", ProposalLevel.class);
+		  List<ProposalLevel> proposalLevel = query.getResultList();
+		  
+		  return proposalLevel;
+	}
+
+}
